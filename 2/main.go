76468b0387f68529e7cb3670c8256f2fcc0ba2bf
Stephen Englert <stevee2112@gmail.com>
2020-09-29 15:17:33 -0400

    finish day 2

--- 2/main.go
+++ 2/main.go
@@ -21,14 +21,17 @@ func main() {
 	scanner := bufio.NewScanner(input)
 
 	totalPaperNeeded := 0;
+	totalRibbonNeeded := 0;
 
 	for scanner.Scan() {
 		dimensions := strings.Split(scanner.Text(), "x")
 
 		totalPaperNeeded += paperNeeded(dimensions[0], dimensions[1], dimensions[2])
+		totalRibbonNeeded += ribbonNeeded(dimensions[0], dimensions[1], dimensions[2])
 	}
 
 	fmt.Printf("Part 1: %d\n", totalPaperNeeded)
+	fmt.Printf("Part 2: %d\n", totalRibbonNeeded)
 }
 
 
@@ -55,3 +58,34 @@ func paperNeeded(ls, ws, hs string) int {
 
 	return (2 * side1) + (2 * side2) + (2 * side3) + smallest
 }
+
+func ribbonNeeded(ls, ws, hs string) int {
+
+	l, _ := strconv.Atoi(ls)
+	w, _ := strconv.Atoi(ws)
+	h, _ := strconv.Atoi(hs)
+
+	box := []int{l, w, h}
+
+	max := 0
+	bow := 1
+	var maxAt int
+	for at, side := range box {
+		bow *= side
+		if side > max {
+			maxAt = at
+			max = side
+		}
+	}
+
+	box = append(box[:maxAt], box[maxAt + 1:]...)
+
+	needed := 0
+
+	for _, side := range box {
+		needed += side + side
+	}
+
+	return needed + bow;
+}
+
