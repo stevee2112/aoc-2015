d93aa651c8d10ef6e99d42a50d1ff0ea19ffe8e8
Stephen Englert <stevee2112@gmail.com>
2020-09-29 14:37:33 -0400

    day 2 part 1 complete

---
+++ 2/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"path"
+    "bufio"
+	"strings"
+	"strconv"
+)
+
+func main() {
+
+	// Get Data
+	_, file, _,  _ := runtime.Caller(0)
+
+	input, _ := os.Open(path.Dir(file) + "/input")
+
+	defer input.Close()
+	scanner := bufio.NewScanner(input)
+
+	totalPaperNeeded := 0;
+
+	for scanner.Scan() {
+		dimensions := strings.Split(scanner.Text(), "x")
+
+		totalPaperNeeded += paperNeeded(dimensions[0], dimensions[1], dimensions[2])
+	}
+
+	fmt.Printf("Part 1: %d\n", totalPaperNeeded)
+}
+
+
+func paperNeeded(ls, ws, hs string) int {
+
+	l, _ := strconv.Atoi(ls)
+	w, _ := strconv.Atoi(ws)
+	h, _ := strconv.Atoi(hs)
+
+	side1 := (l * w)
+	smallest := side1
+
+	side2 := (w * h)
+
+	if side2 < smallest {
+		smallest = side2
+	}
+
+	side3 := (h * l)
+
+	if side3 < smallest {
+		smallest = side3
+	}
+
+	return (2 * side1) + (2 * side2) + (2 * side3) + smallest
+}
